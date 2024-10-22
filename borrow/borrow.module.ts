e776a05149706c4046f510c5a12ab67beb771177
Ethan DIVET <ethan.divet@bimeo.fr>
2024-10-22 12:37:59 +0200

    True start

--- borrow/borrow.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BorrowService } from './borrow.service';
-import { BorrowController } from './borrow.controller';
-
-@Module({
-  providers: [BorrowService],
-  controllers: [BorrowController]
-})
-export class BorrowModule {}
